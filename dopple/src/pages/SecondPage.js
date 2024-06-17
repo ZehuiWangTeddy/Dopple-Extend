fe50fecbfc3550b85f97e3772d8e3bb1b1956f88
anastasialukanova <anastasia.lukanova@student.nhlstenden.com>
2024-06-18 00:16:34 +0200

    Fixed functionality of config page

--- dopple/src/pages/SecondPage.js
+++ dopple/src/pages/SecondPage.js
@@ -12,15 +12,36 @@ import { useFeature } from '../contexts/FeatureContext';
 const SecondPage = ({ handleNavigate }) => {
   const { features } = useFeature();
 
+  const gridTemplateAreas = () => {
+    const areas = [];
+    if (features.OrderStatus) areas.push('orderStatus');
+    if (features.PrinterStatus) areas.push('printers');
+    if (features.OperatorStatus) areas.push('operatorStatus');
+    if (features.NetworkServices) areas.push('networkServices');
+
+    if (areas.length === 1) {
+      return `'${areas[0]} ${areas[0]} ${areas[0]} ${areas[0]} ${areas[0]} ${areas[0]}' ' ${areas[0]} ${areas[0]} ${areas[0]} ${areas[0]} ${areas[0]} ${areas[0]}' ' ${areas[0]} ${areas[0]} ${areas[0]} ${areas[0]} ${areas[0]} ${areas[0]}' ' ${areas[0]} ${areas[0]} ${areas[0]} ${areas[0]} ${areas[0]} ${areas[0]}'`;
+    }
+    if (areas.length === 2) {
+      return `'${areas[0]} ${areas[0]} ${areas[0]} ${areas[0]} ${areas[1]} ${areas[1]}' '${areas[0]} ${areas[0]} ${areas[0]} ${areas[0]} ${areas[1]} ${areas[1]}' '${areas[0]} ${areas[0]} ${areas[0]} ${areas[0]} ${areas[1]} ${areas[1]}' '${areas[0]} ${areas[0]} ${areas[0]} ${areas[0]} ${areas[1]} ${areas[1]}'`;
+    }
+    if (areas.length === 3) {
+      return `'${areas[0]} ${areas[0]} ${areas[0]} ${areas[0]} ${areas[1]} ${areas[1]}' '${areas[0]} ${areas[0]} ${areas[0]} ${areas[0]} ${areas[1]} ${areas[1]}' '${areas[2]} ${areas[2]} ${areas[2]} ${areas[2]} ${areas[2]} ${areas[2]}' '${areas[2]} ${areas[2]} ${areas[2]} ${areas[2]} ${areas[2]} ${areas[2]}'`;
+    }
+    return `'orderStatus orderStatus orderStatus orderStatus printers printers' 'orderStatus orderStatus orderStatus orderStatus printers printers' 'operatorStatus operatorStatus operatorStatus operatorStatus networkServices networkServices' 'operatorStatus operatorStatus operatorStatus operatorStatus networkServices networkServices'`;
+  };
+
   return (
     <DashhboardContextProvider>
       <div className="dashboard-container">
-        <DoppleHeader />
-        <div className="second-content">
-          {features.OrderStatus && <OrderStatus />}
-          {features.PrinterStatus && <PrinterStatus />}
-          {features.OperatorStatus && <ChiselServer />}
-          {features.NetworkServices && <NetworkStatus />}
+        <div className="header">
+          <DoppleHeader />
+        </div>
+        <div className="second-content" style={{ gridTemplateAreas: gridTemplateAreas() }}>
+          {features.OrderStatus && <div className="grid-item" style={{ gridArea: 'orderStatus' }}><OrderStatus /></div>}
+          {features.PrinterStatus && <div className="grid-item" style={{ gridArea: 'printers' }}><PrinterStatus /></div>}
+          {features.OperatorStatus && <div className="grid-item" style={{ gridArea: 'operatorStatus' }}><ChiselServer /></div>}
+          {features.NetworkServices && <div className="grid-item" style={{ gridArea: 'networkServices' }}><NetworkStatus /></div>}
         </div>
         <div className="button-container">
           <PreviousPageButton handleNavigate={() => handleNavigate(1)} />
