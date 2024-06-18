3c175fb938b63edc8b5649d297d146ff07b8755a
Maksym <maksym.bivol@student.nhlstenden.com>
2024-06-19 00:16:07 +0200

    Auto switch fix + improvements(doesnt switch if any action is performed by user or any camera is on focus)

--- dopple/src/components/PageSwitcher.js
+++ dopple/src/components/PageSwitcher.js
@@ -1,20 +1,58 @@
 // src/components/PageSwitcher.js
 import { useEffect, useRef, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 const PageSwitcher = () => {
   const navigate = useNavigate();
-  const doorbellSoundRef = useRef(null); // Ref for doorbell sound
+  const location = useLocation();
   const [focusTimeout, setFocusTimeout] = useState(null);
+  const [userActionTimeout, setUserActionTimeout] = useState(null);
   const eventSourceRef = useRef(null);
+  const autoSwitchIntervalRef = useRef(null);
 
   useEffect(() => {
-    const intervalId = setInterval(() => {
-      navigate((prev) => (prev === '/' ? '/stats' : '/'));
-    }, 15000); // 15000 milliseconds = 15 seconds
+    const startAutoSwitchTimer = () => {
+      autoSwitchIntervalRef.current = setInterval(() => {
+        if (!focusTimeout && !userActionTimeout) { // check if any camera isnt on focus or user performed any action
+          navigate(location.pathname === '/' ? '/stats' : '/');
+        }
+      }, 15000); // 15000 milliseconds = 15 seconds
+    };
 
-    return () => clearInterval(intervalId);
-  }, [navigate]);
+    startAutoSwitchTimer();
+
+    return () => {
+      if (autoSwitchIntervalRef.current) {
+        clearInterval(autoSwitchIntervalRef.current);
+      }
+    };
+  }, [navigate, location.pathname, focusTimeout, userActionTimeout]);
+
+  useEffect(() => {
+    const resetUserActionTimeout = () => {
+      if (userActionTimeout) {
+        clearTimeout(userActionTimeout);
+      }
+      setUserActionTimeout(setTimeout(() => setUserActionTimeout(null), 30000)); // 30000 milliseconds = 30 seconds
+    };
+
+    const handleUserAction = () => {
+      resetUserActionTimeout();
+    };
+
+    window.addEventListener('click', handleUserAction);
+    window.addEventListener('keydown', handleUserAction);
+    window.addEventListener('mousemove', handleUserAction);
+
+    return () => {
+      window.removeEventListener('click', handleUserAction);
+      window.removeEventListener('keydown', handleUserAction);
+      window.removeEventListener('mousemove', handleUserAction);
+      if (userActionTimeout) {
+        clearTimeout(userActionTimeout);
+      }
+    };
+  }, [userActionTimeout]);
 
   useEffect(() => {
     eventSourceRef.current = new EventSource('http://localhost:3000/api/subscribe');
@@ -24,6 +62,9 @@ const PageSwitcher = () => {
       console.log(eventMsg);
 
       if (state === 'ON') {
+        if (focusTimeout) {
+          clearTimeout(focusTimeout);
+        }
 
         navigate('/');
 
